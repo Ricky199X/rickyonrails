2d1cbc973f3cb9b74e38e52acab1ac366552dffc
Ricky199X <rickyrojas19@gmail.com>
2020-04-15 09:10:54 -0400

    adds basic nav - need to figure out how to get it to work with react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <h1 class="deep-purple-text darken1">hello this is the navbar component</h1>
+    <nav class="green">
+      <div class="nav-wrapper">
+        <div class="container">
+          <a href="#" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-small-and-down">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 };
 
