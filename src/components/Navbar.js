861e33acb8ad651c47c6d52c785090427b8ac795
Ricky199X <rickyrojas19@gmail.com>
2020-04-16 07:43:33 -0400

    installs materialize css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,25 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav class="green">
-      <div class="nav-wrapper">
-        <div class="container">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-small-and-down">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    // <nav class="green">
+    //   <div class="nav-wrapper">
+    //     <div class="container">
+    //       <a href="#" class="brand-logo">Logo</a>
+    //       <ul id="nav-mobile" class="right hide-on-small-and-down">
+    //         <li>
+    //           <a href="#">Home</a>
+    //         </li>
+    //         <li>
+    //           <a href="#">About</a>
+    //         </li>
+    //         <li>
+    //           <a href="#">Contact</a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+    <h1>This is the navbar</h1>
   )
 };
 
