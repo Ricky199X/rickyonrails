6af208294f76d58b7d2d3245a9b384f37da95147
Ricky199X <rickyrojas19@gmail.com>
2020-04-16 10:46:56 -0400

    adds react router dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import M from 'materialize-css'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Imported Components
 import Navbar from './components/Navbar';
@@ -11,8 +11,16 @@ import Footer from './components/Footer'
 class App extends React.Component {
   render() {
     return (
-      <Navbar />
-      // <Footer />
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
