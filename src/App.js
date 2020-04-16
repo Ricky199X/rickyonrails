6a71c0b99d39c0371436557aabee6034641cd0b6
Ricky199X <rickyrojas19@gmail.com>
2020-04-16 08:08:19 -0400

    adds backgrround image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import M from 'materialize-css'
+
 
 // Imported Components
 import Navbar from './components/Navbar';
 
 class App extends React.Component {
   render() {
-    return <Navbar />;
+    return (
+      <Navbar />
+    )
   }
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
+
 const Navbar = () => {
   return (
-    // <nav class="green">
-    //   <div class="nav-wrapper">
-    //     <div class="container">
-    //       <a href="#" class="brand-logo">Logo</a>
-    //       <ul id="nav-mobile" class="right hide-on-small-and-down">
-    //         <li>
-    //           <a href="#">Home</a>
-    //         </li>
-    //         <li>
-    //           <a href="#">About</a>
-    //         </li>
-    //         <li>
-    //           <a href="#">Contact</a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
-    <h1>This is the navbar</h1>
+    <nav class="transparent">
+      <div class="nav-wrapper">
+        <div class="container">
+          <a href="#" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-small-and-down">
+            <li>
+              <a href="#" class="black-text">Home</a>
+            </li>
+            <li>
+              <a href="#" class="black-text">About</a>
+            </li>
+            <li>
+              <a href="#" class="black-text">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    // <h1>This is the navbar</h1>
   )
 };
 
