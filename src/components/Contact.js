0913a45db2439ebf23a09a99f5c5ce2add88a612
Ricky199X <rickyrojas19@gmail.com>
2020-04-17 15:50:28 -0400

    white icons now appearing on star background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,34 +2,37 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div class="section white">
-            <div class="container">
+        <div class="contact-page-container">
+            <div class="message-container ">
+                <h1 class="center-align white-text">Contact</h1>
+                <h3 class="center-align white-text">Want to chat? Let's connect!</h3>
+            </div>
+            <div class="icons">
+
                 <div class="row">
                     <div className="col s12">
-                        <h3 class="center-align">Contact</h3>
-                        <h3 class="center-align">Want to chat? Let's connect!</h3>
 
                         {/* Icons Container */}
-                        <div class="icons-container">
+                        <div class="icons-container black">
                             <a href="https://twitter.com/rickyonrails"
                                 target="https://twitter.com/rickyonrails"
-                                class="black-text">
+                                class="white-text">
                                 <i class="fab fa-twitter fa-4x"></i>
                             </a>
                             <a href="https://www.linkedin.com/in/ricky-rojas-92096965/"
                                 target="https://www.linkedin.com/in/ricky-rojas-92096965/"
-                                class="black-text">
+                                class="white-text">
                                 <i class="fab fa-linkedin fa-4x"></i>
                             </a>
                             <a href="https://github.com/Ricky199X"
                                 target="https://github.com/Ricky199X"
-                                class="black-text">
+                                class="white-text">
                                 <i class="fab fa-github fa-4x"></i>
                             </a>
 
                             <a href="https://medium.com/@rickyrojas19"
                                 target="https://medium.com/@rickyrojas19"
-                                class="black-text">
+                                class="white-text">
                                 <i class="fab fa-medium fa-4x"></i>
                             </a>
                         </div>
