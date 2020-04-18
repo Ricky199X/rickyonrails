8b0fd1b7e129a72e1b67d687ec373d8baec423cb
Ricky199X <rickyrojas19@gmail.com>
2020-04-18 08:48:07 -0400

    adds basic structure for projects page - project divs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,9 +9,15 @@ class Projects extends React.Component {
                     <h1 class="white-text">Projects Page</h1>
                 </div>
                 <div class="projects">
-                    <div class="project1"></div>
-                    <div class="project2"></div>
-                    <div class="project3"></div>
+                    <div class="project1" >
+                        <h4 class="white-text right-align">Gigfreq</h4>
+                    </div>
+                    <div class="project2">
+                        <h3 class="white-text">Record Collector</h3>
+                    </div>
+                    <div class="project3">
+                        <h3 class="white-text">Quiz-It</h3>
+                    </div>
                 </div>
             </div>
         )
