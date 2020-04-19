34fdd09f68f316ff2a8558a23c70aa1f9012161e
Ricky199X <rickyrojas19@gmail.com>
2020-04-19 11:45:25 -0400

    adds skills container - will begin adding icons for skills

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,11 +4,11 @@ const AboutPage = () => {
     return (
         <div class="container about-page">
 
-            <div class="about-page-header center">
-                <h1 class="white-text">About Me</h1>
-            </div>
-
             <section id="about">
+                <div class="about-page-header center">
+                    <h1 class="white-text">About Me</h1>
+                </div>
+
                 <div class="container">
                     <div class="row">
                         <div class="col s12 center">
@@ -21,14 +21,37 @@ const AboutPage = () => {
                             <p class="white-text">Software Engineer</p>
 
                             {/* About Blurb */}
-                            <p class="white-text">Hi, I'm Ricky, a Software Engineer with experience in Ruby on Rails, JavaScript, React.js, and Redux.
+                            <p class="white-text">Hi, I'm Ricky! I'm a Software Engineer with experience in Ruby on Rails, JavaScript, React.js, and Redux.
                             I am a lifelong tech enthusiast who loves to build products and immersive experiences for others. I am currently looking for an opportunity to work with like-minded individuals where I can continue to learn and grow as a tech professional.
                             Drawing on my background in music, I leverage my creativity and engineering skills to bring my visions to life.
                             </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Skills Section */}
+            <section id="skills">
+                <h1 class="white-text center">Developer Toolkit</h1>
+
+                <div class="container">
+                    <div class="row text-center">
+                        <div className="col s4">
+
+                        </div>
+                        <div className="col s4">
+
+                        </div>
+                        <div className="col s4">
+
+                        </div>
+                        <div className="col s4">
 
                         </div>
                     </div>
                 </div>
+
+
 
             </section>
 
