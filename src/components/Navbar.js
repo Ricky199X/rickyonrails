228f936bf2eba587447b9a65d0ceaf9a8f2bd0cb
Ricky199X <rickyrojas19@gmail.com>
2020-04-19 23:59:24 -0400

    active class on navbar links working opposite from nteded behavior - need to fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,25 @@ import { Link, withRouter } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav class="transparent">
-      <div class="container">
-        <Link to='/' class="brand-logo left-align">Ricky Rojas</Link>
-        <ul id="nav-mobile" class="right hide-on-small-and-down">
-          <li>
-            <Link to='/about' class="white-text">About</Link>
-          </li>
-          <li>
-            <Link to='/projects' class="white-text">Projects</Link>
-          </li>
-          <li>
-            <Link to='/contact' class="white-text">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="nav">
+      <nav class="transparent">
+        <div class="container">
+          <Link to='/' class="brand-logo left-align">Ricky Rojas</Link>
+          <ul id="nav-mobile" class="right hide-on-small-and-down ">
+            <li>
+              <Link to='/about' class="white-text active">About</Link>
+            </li>
+            <li>
+              <Link to='/projects' class="white-text active">Projects</Link>
+            </li>
+            <li>
+              <Link to='/contact' class="white-text active">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
   )
 };
 
