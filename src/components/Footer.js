6f05f9c03fb8f0911ef1aaf5789ca0cba839a46b
Ricky199X <rickyrojas19@gmail.com>
2020-04-21 07:47:39 -0400

    added social icons to footer, need to make sure alignment is correct

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,34 @@ const Footer = () => {
                 <div class="row">
                     <div class="col l6 s12">
                         <h5 class="white-text">Ricky Rojas &copy; 2020</h5>
+
+
+                    </div>
+
+                    <div class="col l6 s12">
+                        <ul class="list-inline">
+                            <li>
+                                <a href="https://twitter.com/rickyonrails"
+                                    target="https://twitter.com/rickyonrails"
+                                    class="white-text">
+                                    <i class="fab fa-twitter fa-2x"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/ricky-rojas-92096965/"
+                                    target="https://www.linkedin.com/in/ricky-rojas-92096965/"
+                                    class="white-text">
+                                    <i class="fab fa-linkedin fa-2x"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/Ricky199X"
+                                    target="https://github.com/Ricky199X"
+                                    class="white-text">
+                                    <i class="fab fa-github fa-2x"></i>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
