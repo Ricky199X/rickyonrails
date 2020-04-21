709f2751c56e5ed9b362200a808bc532be2d839b
Ricky199X <rickyrojas19@gmail.com>
2020-04-21 15:20:38 -0400

    added a sticky footer to each page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,12 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <section id="home">
-            <div class="container home-container">
-                <p class="white-text flow-text">Hi, I'm Ricky! I'm a Brooklyn-based software engineer with experience in Ruby on Rails, JavaScript, React.js, and Redux.
-                I am a lifelong tech enthusiast who loves to build products and immersive experiences for others.
-                I am currently looking for an opportunity to work with like-minded individuals
-                where I can continue to learn and grow as a tech professional.
-                Drawing on my background in music, I leverage my creativity and engineering skills
-                to bring my visions to life.
-            </p>
+        <div class="text-align center">
+            <div class="home-container">
+                <h1 class="home-message white-text center">Enter</h1>
             </div>
+        </div>
 
-        </section>
     )
 }
 
