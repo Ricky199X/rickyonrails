bef2bce06fe6ebc8e794f91be5fe0d97522baf04
Ricky199X <rickyrojas19@gmail.com>
2020-04-21 16:48:25 -0400

    adds pulse button to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
     return (
         <div class="text-align center">
             <div class="home-container">
-                <h1 class="home-message" id="home-message">Enter</h1>
+                <h1 class="home-message pulse" id="home-message">Welcome!</h1>
+                <a class="btn-floating btn-large cyan lighten-4 pulse"><i class="material-icons black-text">double_arrow</i></a>
             </div>
         </div>
 
