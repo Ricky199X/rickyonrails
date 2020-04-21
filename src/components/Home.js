bef72080b7d34dd1ed0f18f6eb55af011cb60c04
Ricky199X <rickyrojas19@gmail.com>
2020-04-21 17:00:18 -0400

    added local state to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-const Home = () => {
-    return (
-        <div class="text-align center">
-            <div class="home-container">
-                <h1 class="home-message pulse" id="home-message">Welcome!</h1>
-                <a class="btn-floating btn-large cyan lighten-4 pulse"><i class="material-icons black-text">double_arrow</i></a>
+class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            shouldRedirect: false
+        }
+    }
+
+    // check for if redirect should occur
+    // componentDidUpdate(prevProps) {
+    //     if (!prevProps && this.props.user.loggedIn) {
+    //         this.setState({
+    //             shouldRedirect: true
+    //         })
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     console.log(this.state)
+    // }
+
+    render() {
+
+
+        return (
+            <div class="text-align center">
+                <div class="home-container">
+                    <h1 class="home-message pulse" id="home-message">Welcome!</h1>
+                    <a class="btn-floating btn-large cyan lighten-4 pulse"><i class="material-icons black-text">double_arrow</i></a>
+                </div>
             </div>
-        </div>
 
-    )
+        )
+    }
+
 }
 
 export default Home
